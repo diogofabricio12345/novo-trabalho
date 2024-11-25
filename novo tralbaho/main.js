5652ae93480d00a1d18cdc6da22fa3747c4738c5
diogofabricio12345 <128058085+diogofabricio12345@users.noreply.github.com>
2024-11-25 11:41:30 -0300

    Add files via upload

--- novo tralbaho/main.js
+++ novo tralbaho/main.js
@@ -1,5 +1,37 @@
-const numeroSenha = document.queryselector('.parametro-senha-texto');
-let tamanhoSenha = 12;
+const numeroSenha = document.querySelector(".parametro-senha-texto");
+let tamanhoSenha = 666;
 numeroSenha.textcontent = tamanhoSenha;
-const botoes = document.querySelector('.parametro-senha-botoes');
+const botoes = document.querySelectorAll(".parametro-senha-botao");
+const campoSenha = document.querySelector("#campo-senha");
+const letrasMaiusculas = "ABCDEFGHIJKLMNOPQRSTUVWYZ";
+botoes[1].onclick = aumentartamanho;
+geraSenha();
+
 botoes[0].onclick = diminuirTamanho;
+botoes[1].onclick = aumentartamanho
+function diminuirTamanho(){
+    if(tamanhoSenha>6){
+    tamanhoSenha=tamanhoSenha-1;
+    numeroSenha.textContent=tamanhoSenha;
+}
+}
+
+function aumentartamanho(){
+    if(tamanhoSenha<6){
+    tamanhoSenha=tamanhoSenha+1;
+    numeroSenha.textContent=tamanhoSenha;
+    }
+}
+function geraSenha(){
+    let senha = "";
+    for(let i=0;i<tamanhoSenha; i++){
+let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+numeroAleatorio = Math.floor(numeroAleatorio);
+senha = senha + letrasMaiusculas[numeroAleatorio];
+
+ 
+
+}
+campoSenha.value = senha;
+}
+
